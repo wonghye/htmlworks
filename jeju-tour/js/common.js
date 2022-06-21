184a7d8d15d477396456025051fa72acd2ae6797
wonghye <dearhw117@naver.com>
2022-06-21 14:52:43 +0900

    0621

--- jeju-tour/js/common.js
+++ jeju-tour/js/common.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-
     //모바일 메뉴 띄움
-    $('.openMOgnb').click(function(){
-        $('header').addClass('on');
-        $('header .header_cont').slideDown('fast');
-        $('header .header_area .header_cont .closePop').show();			
+    let isOpen = false;
+    $('.openMOgnb').click(function(){  // 메뉴를 클릭하면
+        if(isOpen == false){  
+            $('header').addClass('on');
+            $('header .header_cont').slideDown('fast');
+            $('header .header_area .header_cont .closePop').show();	
+            isOpen = true; // 상태 변경
+        }else{ // isOpen == true
+            $('header .header_cont').slideUp('fast');
+            $('header').removeClass('on');
+            isOpen = false;
+        }
     });
+
+    //닫기 버튼 클릭
     $('header .header_cont .closePop').click(function(){
         $('.header_cont').slideUp('fast');
         $('header').removeClass('on');
     });
-})
+});
 
 //창을 늘리고 줄였을때 메뉴바 설정
 $(function(){
@@ -21,5 +30,5 @@ $(function(){
         else{
             $('.header_cont').hide();
         }
-    })
-})
+    });
+});
